4a4bf8d86eb55da57062961da76d2b699345f063
pedrovitor <pedro.vitor@opovodigital.com>
2023-02-21 09:40:18 -0300

    comentando codigo js e ajustes na msg de confirmar exclusao

--- scripts.js
+++ scripts.js
@@ -1,21 +1,5 @@
-document.addEventListener("click", (e) => {
-  const listaClasse = Array.prototype.slice.call(e.target.classList);
-  console.log(e);
-  if (e.target && listaClasse.includes("acessarFilme")) {
-    const url = e.target.dataset.url;
-    window.open(url, "_blank");
-  }
 
-  if (e.target && listaClasse.includes("excluirFilme")) {
-    const response = confirm(`Quer mesmo excluir o filme: ${dados.titulo}?`);
-
-    if (response) {
-      const id = e.target.dataset.id;
-      document.getElementById(id).remove();
-    }
-  }
-});
-
+//VALORES INCIAIS DO OBJETO
 let dados = {
   titulo: "",
   ano: 0,
@@ -23,21 +7,31 @@ let dados = {
   link: "",
 };
 
+
+//FUNCAO PARA ATUALIZAR DADOS
 const atualizarDados = () => {
+  //PEGA O OBJ 'dados' E DÁ UM NOVO VALOR PARA AS PROPRIEDADES
   dados = {
     titulo: document.querySelector("#titulo").value,
     ano: document.querySelector("#ano").value,
     categoria: document.querySelector("#categoria").value,
     link: document.querySelector("#link").value,
   };
+  //ESSA FUNÇÃO É CHAMADA NOS ELEMENTOS NO PROPRIO HTML COM 'onkeyup' E 'onchange'
 
+  //FAZ COM O ELEMENTO PASSE A TER O CONTEUDO DO OBJ 'dados' NO FORMATO JSON
   document.querySelector("#debug").innerHTML = JSON.stringify(dados);
 };
 
+//MONTA O NOVO ITEM DA LISTA
 const montaItem = () => {
+
+  //PEGA O TEMPO JÁ QUE O TEMPO NÃO SE REPETE USAMOS COM 'id'
   const id = new Date().getTime();
 
+  //VARIAVEL QUE VAI CRIAR O HTML DO NOVO ITEM SEMPRE COM NOVOS DADOS
   const item = `
+
         <tr id="${id}">
             <td>${dados.titulo}</td>
             <td>${dados.categoria}</td>
@@ -47,7 +41,8 @@ const montaItem = () => {
             ><button class="btn btn-danger excluirFilme" data-id="${id}">Excluir</button>
             </td>
         <tr>
-    `;
+    `; //TEMOS UM 'tr' QUE VAI TER SEMPRE UM ID DIFERENTE
+       //TEMOS ALGUNS TD's, SEMPRE ATUALIZANDO AS INFORMAÇÕES, SENDO QUE OS 2 ULTIMOS USAM O ATRIBUTO 'data' PARA, FAZERMOS A FUNÇÃO DE ABRIR O LINK DO FILME E EXCLUIR O FILME PELO ID
 
   return item;
 };
@@ -74,3 +69,22 @@ const cadastrar = () => {
 };
 
 document.querySelector("#btn-cad").addEventListener("click", cadastrar);
+
+document.addEventListener("click", (e) => {
+  const listaClasse = Array.prototype.slice.call(e.target.classList);
+  console.log(e);
+  if (e.target && listaClasse.includes("acessarFilme")) {
+    const url = e.target.dataset.url;
+    window.open(url, "_blank");
+  }
+
+  if (e.target && listaClasse.includes("excluirFilme")) {
+    const response = confirm(`Quer mesmo excluir esse filme?`);
+
+    if (response) {
+      const id = e.target.dataset.id;
+      document.getElementById(id).remove();
+    }
+  }
+});
+
